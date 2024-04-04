de6ca1c640784531a54186e2ad79ecfa6df32c31
Yuchen Zhang <yzhang0701@gmail.com>
2024-04-04 11:50:07 -0400

    Mod: update the attestation generation for Rust

--- Rust/attest-gen.sh
+++ Rust/attest-gen.sh
@@ -23,12 +23,24 @@ mkdir -p "$outputDir"
 # Initialize an array to hold all file paths
 declare -a filePaths
 
-# Define additional path segments as variables
-MOD_PATH_SEGMENT="/go/pkg/mod/"
-GO_MOD_FILENAME="/go.mod"
+# Define the base path for Cargo registry sources
+CARGO_REGISTRY_BASE="${HOME}/.cargo/registry/src"
+
+# Dynamically find the actual path, assuming there's only one such directory
+CRATE_PATH_SEGMENT=$(find "$CARGO_REGISTRY_BASE" -maxdepth 1 -type d -name "index.crates.io-*" | head -n 1)/
+
+if [ -z "$CRATE_PATH_SEGMENT" ]; then
+    echo "Cargo registry index directory not found in $CARGO_REGISTRY_BASE."
+    exit 1
+fi
+
+RUST_CARGO_FILENAME="/Cargo.toml"
 
 while IFS= read -r filePath; do
-    fullPath="${HOME}${MOD_PATH_SEGMENT}${filePath}"
+    # Replace "@" with "-" in filePath
+    modifiedFilePath="${filePath//@/-}"
+
+    fullPath="${CRATE_PATH_SEGMENT}${modifiedFilePath}"
     # Add the full path to the filePaths array
     filePaths+=("$fullPath")
 done < "$inputFile"
@@ -36,11 +48,11 @@ done < "$inputFile"
 # Convert the filePaths array to a space-separated string
 materialPaths="${filePaths[*]}"
 
-# echo "$materialPaths"
+echo "$materialPaths"
 
 # Define the step name as "build"
 stepPerformed="build"
 
-# Run in-toto-run with all file paths, product path, and "go build" as the command
-in-toto-run -m $materialPaths -n "$stepPerformed" --signing-key "$signingKey" -d "$outputDir" -p "$productPath" -- go build
+# Run in-toto-run with all file paths, product path, and "cargo build" as the command
+in-toto-run -m $materialPaths -n "$stepPerformed" --signing-key "$signingKey" -d "$outputDir" -p "$productPath" -- cargo build
 
